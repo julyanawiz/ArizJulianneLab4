46eb12b2e99aee8f71a1562bc0ffb2bb05f6ea83
julyanawiz <2015084954@ust-ics.mygbiz.com>
2018-09-29 21:07:05 +0800

    Initial commit

---
+++ app/src/main/java/com/ariz/julianne/MainActivity.java
@@ -0,0 +1,44 @@
+package com.ariz.julianne;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    protected Button mButton;
+    String mString = "This is my First Toast";
+
+    protected Button normal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout);
+
+        mButton = (Button)findViewById(R.id.button);
+        normal = (Button)findViewById(R.id.normal);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), mString,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(normal, "I am normal", Snackbar.LENGTH_LONG);
+                snackbar.show();
+
+
+            }
+        });
+    }
+}
